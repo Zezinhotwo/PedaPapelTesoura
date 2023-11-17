be0e60b8f8084b9a4176ce2c5691986a9088e4a4
Zezinhodois <123456789+kaio.bg12@gmail.com>
2023-11-17 14:32:13 -0300

    Corrigido bug de entrada de dados do usuário

--- scirpt.js
+++ scirpt.js
@@ -9,6 +9,7 @@ function getComputerChoice() {
         return "Papel";
     }
 }
+// Jogo comparação de escolhas do Usuário
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
@@ -18,14 +19,33 @@ function playRound(playerSelection, computerSelection) {
         (playerSelection === "Pedra" && computerSelection === "Tesoura") ||
         (playerSelection === "Tesoura" && computerSelection === "Papel")
     ) {
+
         return `${playerSelection} Ganha de ${computerSelection}`;
-    } else {
+    } else if (
+        (computerSelection === "Papel" && playerSelection === "Pedra") ||
+        (computerSelection === "Pedra" && playerSelection === "Tesoura") ||
+        (computerSelection === "Tesoura" && computerSelection === "Papel")
+    ) {
+
         return `${playerSelection} Perde para ${computerSelection}`;
+    }
+}
+//contagem de rodadas e Resultado
+function game() {
+    let count = 0; // Inicializa a contagem
+    while (count < 4) {
+        const playerSelection = prompt("Digre Pedra Papel ou Tesoura");
+        const computerSelection = getComputerChoice();
+        console.log("Jogador:", playerSelection);
+        console.log("Computador:", computerSelection);
+        console.log(playRound(playerSelection, computerSelection, count));
+        count++;
     }
 }
 
 const playerSelection = prompt("Digre Pedra Papel ou Tesoura");
-console.log("Jogador:", playerSelection);
 const computerSelection = getComputerChoice();
+console.log("Jogador:", playerSelection);
 console.log("Computador:", computerSelection);
 console.log(playRound(playerSelection, computerSelection));
+console.log(game());
